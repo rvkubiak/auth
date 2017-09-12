6db6a74de538950662bb50c4cae661435f7ab557
Ryan Kubiak <rkubiak@google.com>
2017-09-12 15:22:54 -0700

    Add flag for passing GCS path to push-debian

--- bin/push-debian.sh
+++ bin/push-debian.sh
@@ -9,19 +9,21 @@
 function usage() {
   echo "$0 \
     -c <bazel config to use> \
+    -p <bucket path, e.g. gs://istio-release/release/deb> \
     -v <istio version number>"
   exit 1
 }
 
-while getopts ":c:v:" arg; do
+while getopts ":c:p:v:" arg; do
   case ${arg} in
     c) BAZEL_ARGS="--config=${OPTARG}";;
+    p) GCS_PATH="${OPTARG}";;
     v) ISTIO_VERSION="${OPTARG}";;
     *) usage;;
   esac
 done
 
-if [ -z "${BAZEL_ARGS}" ] || [ -z "${ISTIO_VERSION}" ]; then
+if [ -z "${BAZEL_ARGS}" ] || [ -z "${ISTIO_VERSION}" ] || [ -z "${GCS_PATH}" ]; then
   usage
 fi
 
@@ -30,4 +32,4 @@ set -ex
 bazel ${BAZEL_STARTUP_ARGS} build ${BAZEL_ARGS} "//tools/deb:istio-auth-node-agent"
 gsutil -m cp -r \
   bazel-bin/tools/deb/istio-auth-node-agent_${ISTIO_VERSION}_amd64.* \
-  gs://istio-release/releases/${ISTIO_VERSION}/deb
+  ${GCS_PATH}
